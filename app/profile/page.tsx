e0ae22d15b881619366044f547721e34627059dd
Harshittomar <tomarharshit2023@gmail.com>
2025-07-12 22:21:01 +0530

    Fixed the code for upload image

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -43,7 +43,7 @@ export default function ProfilePage() {
       .upload(fileName, file);
 
     if (error) {
-      console.error("Image upload error:", error.message);
+      console.error("Image upload error!:", error.message);
       return null;
     }
 
