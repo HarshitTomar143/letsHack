56025efe6e1860ff744c06c8930d44d275397082
Harshittomar <tomarharshit2023@gmail.com>
2025-07-27 22:42:24 +0530

    made new /lib/authOptions

--- app/api/requests/recieved/route.ts
+++ app/api/requests/recieved/route.ts
@@ -1,6 +1,6 @@
 import { supabase } from "@/lib/supabaseServer";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../auth/[...nextauth]/route";
+import { authOptions } from "@/lib/authOptions";
 
 export async function GET(){
     const session= await getServerSession(authOptions);
